9cea2f001249602991558df90c3487e310922a41
pledgedavis <pledgedavis14@gmail.com>
2020-01-06 21:01:16 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,16 +2,17 @@
 class Person
   def intialize(name)
      @name = name
-  # beyonce = Person.new("Beyonce")
-  def name
-     @name
-   # def beyonce
-  #     # @beyonce
-    def name=(new_name)
-      @name = new_name
    end
-  end
-end
+  # beyonce = Person.new("Beyonce")
+#   def name
+#     @name
+#   # def beyonce
+#   #     # @beyonce
+#     def name=(new_name)
+#       @name = new_name
+#   end
+#   end
+# end
 end
 # beyonce = Person.new("Beyonce")
 # lassie = Dog.new("Collie")
