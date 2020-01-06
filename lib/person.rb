a81aba7a24f47ff907d8c8c4188a9c86a64f5757
pledgedavis <pledgedavis14@gmail.com>
2020-01-06 21:02:12 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,30 @@
+
+class Person
+  def initialize(name)
+     @name = name
+   end
+  # beyonce = Person.new("Beyonce")
+#   def name
+#     @name
+#   # def beyonce
+#   #     # @beyonce
+#     def name=(new_name)
+#       @name = new_name
+#   end
+#   end
+# end
+end
+# beyonce = Person.new("Beyonce")
+# lassie = Dog.new("Collie")
+ 
+# lassie.breed #=> "Collie"
+
+
+
+
+
+
+
+
+
+
