0fc3d8cef7cc30376dd7e5b592e3ea7ebbd85f06
pledgedavis <pledgedavis14@gmail.com>
2020-01-06 20:52:25 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,29 @@
+
+class Person
+  def intialize(name)
+     @name = name
+  # beyonce = Person.new("Beyonce")
+  def name
+     @name
+   # def beyonce
+  #     # @beyonce
+    def name=(new_name)
+      @name = new_name
+   end
+  end
+end
+end
+# beyonce = Person.new("Beyonce")
+# lassie = Dog.new("Collie")
+ 
+# lassie.breed #=> "Collie"
+
+
+
+
+
+
+
+
+
+
